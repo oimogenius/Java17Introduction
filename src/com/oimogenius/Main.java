c77f644b3c86c011348bc15d6c90dfcac19bd4bd
oimogenius <oimo.genius@gmail.com>
2024-02-19 21:12:44 -0800

    プロジェクトの構成 / Project Structure

--- src/com/oimogenius/Main.java
+++ src/com/oimogenius/Main.java
@@ -1,6 +1,17 @@
 package com.oimogenius;
 
 public class Main {
+
+    // これがコメント
+    /*
+    こうすることで複数行書ける。
+    プログラムには影響がないので、メモとして使える。
+     */
+
+//    Ctrl+/でコメントアウトできる
+
+    public static final String NAME = "おいも";
+
     public static void main(String[] args) {
         System.out.println("Mod開発で使える！ Java知識");
     }
---
+++ src/com/oimogenius/test/Main.java
@@ -0,0 +1,5 @@
+package com.oimogenius.test;
+
+public class Main {
+    // パッケージが違えば、同じ名前のクラスを作ることができる
+}
